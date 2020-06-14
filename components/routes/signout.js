98d2c2104fa1fc619ab13b4cf8508e3842e7ce5c
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2020-06-14 18:29:57 +0530

    cleaned up signout code

--- components/routes/signout.js
+++ components/routes/signout.js
@@ -2,14 +2,10 @@ var admin = require("firebase-admin");
 const Router = require("express").Router()
 
 Router.get("/",(req,res)=>{
-        admin.auth().verifyIdToken(req.headers.authtoken)
-        .then((decodedToken) => {
-                console.log(decodedToken)
-                admin.auth().revokeRefreshTokens(decodedToken.uid)
-                .then(()=>{
-                        console.log("refresh tokens revoked for ",decodedToken.uid)
-                        res.status(200).send({message:"signed out"})
-                })
-        })  
+        admin.auth().revokeRefreshTokens(req.userId)
+        .then(()=>{
+                console.log("refresh tokens revoked for ",req.userId)
+                res.status(200).send({message:"signed out "+req.userId})
+        })
 })
 module.exports = Router
