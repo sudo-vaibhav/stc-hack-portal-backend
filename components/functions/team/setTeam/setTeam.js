8050f8eca9c736c40b84c58b08a637c82af400bd
Vaibhav Chopra <vaibhavchopra@Vaibhavs-MacBook-Air.local>
2020-06-26 03:42:48 +0530

    added prevention of double team creation for same event by a user

---
+++ components/functions/team/setTeam/setTeam.js
@@ -0,0 +1,55 @@
+const Team = require("../../../models/Team")
+const mongoose = require("mongoose")
+const getUser = require("../../user/profile/getUser")
+const getEvent = require("../../event/getEvent")
+const setTeam = async (req, res) => {
+
+    const eventQuery = await getEvent(req.body.eventId, "byId")
+    if (eventQuery.status == 200) {
+        const creatorQuery = await getUser(req.userId, "byId")
+
+        //this will check if user has completed their profile or not
+        if (creatorQuery.status == 200) {
+
+            //check if user doesn't already have a team in same event already
+            Team.findOne({
+                    members: req.userId,
+                    eventId: req.body.eventId
+                })
+                .then(otherTeam => {
+                    if (otherTeam) {
+                        return res.status(400).send({
+                            message: "You already have a team for the same event"
+                        })
+                    }
+                    else{
+                        const team = new Team({
+                            _id: new mongoose.Types.ObjectId().toString(),
+                            creatorId: req.userId,
+                            teamName: req.body.teamName,
+                            eventId: req.body.eventId,
+                            description: req.body.description,
+                            members: [req.userId],
+                            skillsRequired: req.body.skillsRequired || [],
+                            pendingRequests: []
+                        })
+                        team.save()
+                            .then(async (newTeam) => {
+                                const creator = creatorQuery.payload
+                                creator.teams.push(newTeam._id)
+                                await creator.save()
+                                return res.status(200).send(newTeam)
+                            })
+                    }
+                })
+
+        } else {
+            return res.status(creatorQuery.status).send(creatorQuery.message)
+        }
+
+    } else {
+        return res.status(eventQuery.status).send(eventQuery.payload)
+    }
+}
+
+module.exports = setTeam
--- components/routes/teams.js
+++ components/routes/teams.js
@@ -12,7 +12,7 @@ const cancelInvite = require("../functions/team/invite/cancelInvite")
 const getTeam = require("../functions/team/getTeam/getTeam")
 const removeMember = require("../functions/team/invite/removeMember")
 const getTeamInfoByAccessLevel = require("../functions/team/getTeam/getTeamInfoByAccessLevel")
-
+const setTeam = require("../functions/team/setTeam/setTeam")
 //authentication middleware
 const {
     checkAuth
@@ -39,52 +39,8 @@ Router.get("/:teamId", checkAuth, async (req, res) => {
     }
 })
 
-Router.post("/setteam", checkAuth, (req, res) => {
-    Event.findById(req.body.eventId).select('-__v')
-        .then(async (event) => {
-            if (!event) {
-                return res.status(404).send({
-                    message: "No data found for this event"
-                })
-            } else {
-                const creatorQuery = await getUser(req.userId, "byId")
-
-                //this will check if user has completed their profile or not
-                if (creatorQuery.status == 200) {
-                    const team = new Team({
-                        _id: new mongoose.Types.ObjectId().toString(),
-                        creatorId: req.userId,
-                        teamName: req.body.teamName,
-                        eventId: req.body.eventId,
-                        description: req.body.description,
-                        members: [req.userId],
-                        skillsRequired: req.body.skillsRequired || [],
-                        pendingRequests: []
-                    })
-                    team.save()
-                        .then(async (newTeam) => {
-                            const creator = creatorQuery.payload
-                            creator.teams.push(newTeam._id)
-                            await creator.save()
-                            return res.status(200).send(newTeam)
-                        })
-                } else {
-                    return res.status(creatorQuery.status).send(creatorQuery.message)
-                }
-
-            }
-
-
-        })
-        .catch(err => {
-            return res.status(500).send({
-                error: "Internal Server Error"
-            })
-        })
-})
-
+Router.post("/setteam", checkAuth, setTeam)
 Router.post("/sendinvite", checkAuth, sendInvite)
-
 Router.post("/cancelinvite", checkAuth, cancelInvite)
 Router.post("/removemember", checkAuth, removeMember)
 
