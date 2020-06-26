3d49e903d133f5194efa33ac5f4a30a676c0af3f
Vaibhav Chopra <vaibhavchopra@Vaibhavs-MacBook-Air.local>
2020-06-27 02:01:30 +0530

    improved populate user with team efficiency

--- components/functions/user/profile/populateUserWithTeamInfo/populateUserWithTeamInfo.js
+++ components/functions/user/profile/populateUserWithTeamInfo/populateUserWithTeamInfo.js
@@ -5,26 +5,26 @@ const populateUserWithTeamInfo = async (user)=>{
     const userObject = {...user.toJSON()}
     const teamInfoQueries = []
     const inviteInfoQueries = []
-    for await(const team of userObject.teams){
-        teamInfoQueries.push(await getTeam(team,"byId"))  //this will return promises, we will await for their resolution later
+    for (const team of userObject.teams){
+        teamInfoQueries.push( getTeam(team,"byId"))  //this will return promises, we will await for their resolution later
     }
 
-    for await (const invite of userObject.invites){
-        inviteInfoQueries.push(await getTeam(invite, "byId")) //this will return promises, we will await for their resolution later
+    for  (const invite of userObject.invites){
+        inviteInfoQueries.push( getTeam(invite, "byId")) //this will return promises, we will await for their resolution later
     }
 
     // await Promise.all([...teamInfoQueries,...inviteInfoQueries])
 
     const teamsInfo = []
     const invitesInfo = []
-    for (const teamQuery of teamInfoQueries){
+    for await(const teamQuery of teamInfoQueries){
         const {_id,teamName,creatorId} = teamQuery.payload
         teamsInfo.push({_id,teamName,creatorId})
         // console.log("teamquery",teamQuery)
         // teamsInfo.push(teamQuery.payload)
     }
 
-    for (const inviteQuery of inviteInfoQueries)  {   
+    for await (const inviteQuery of inviteInfoQueries)  {   
         const {
             _id,
             teamName,
@@ -41,7 +41,7 @@ const populateUserWithTeamInfo = async (user)=>{
     userObject.teamsInfo = teamsInfo
     userObject.invitesInfo = invitesInfo
 
-    return Promise.resolve(userObject)
+    return userObject
 
 }
 
