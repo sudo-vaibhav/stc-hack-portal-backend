4bc1ff97ad76a549ff23cad56cc8a93a4fe90a76
deepcr7 <deepeshsuranjan@gmail.com>
2020-06-29 12:54:31 +0530

    fixed errors in setting up events

--- components/functions/event/setEvent/setEvent.js
+++ components/functions/event/setEvent/setEvent.js
@@ -1,6 +1,9 @@
 const Event = require("../../../models/Event/Event")
 const mongoose = require("mongoose")
 const multer = require("multer")
+const fileUpload = require("../../upload/fileUpload/fileUpload")
+const fileStorage= require("../../upload/fileStorage/fileStorage")
+const fileFilter = require("../../upload/fileFilter/fileFilter")
 
 const setEvent= async (req, res) => {
   console.log(req.file)
@@ -19,7 +22,10 @@ const setEvent= async (req, res) => {
   //after you tell it that unique:true  in Event schema 😕
   //read more about it here: https://mongoosejs.com/docs/faq.html#unique-doesnt-work
   Event.init().then(() => {
-      const event = new Event({
+    let event = undefined
+    if(req.file)
+    {
+       event = new Event({
           _id: new mongoose.Types.ObjectId().toString(),
           creatorId: req.userId,
           startDate: startDate,
@@ -31,7 +37,13 @@ const setEvent= async (req, res) => {
           minimumTeamSize: minimumTeamSize,
           maximumTeamSize: maximumTeamSize,
           eventImage: "https://hackportal.herokuapp.com/eventImage/" + req.file.filename
-      });
+      })}
+      else 
+      {
+        return res.status(404).send({
+          message: "Hey!, This error could be a result of any of the mistakes you have made- 1. You have not uploaded an Image  2. You have not uploaded the correct Image Format(only JPEG,JPG,SVG and PNG are allowed)  3. Your Image Size is greater than 6MB"
+        })
+      }
       event
           .save()
           .then(result => {
--- components/functions/upload/fileFilter/fileFilter.js
+++ components/functions/upload/fileFilter/fileFilter.js
@@ -3,10 +3,10 @@ const multer = require("multer")
 //to filter files according to mimetype
 const fileFilter = (req, file, cb) => {
   //criteria to accept a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/svg') {
       cb(null, true)
   } else {
-      cb("wrong image format", false)
+      cb(null,false)
   }
 }
 
