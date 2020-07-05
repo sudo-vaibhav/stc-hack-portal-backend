303cf53b84e5e598a89904f5e4e3284585bc174f
Vaibhav Chopra <vaibhavchopra@Vaibhavs-MacBook-Air.local>
2020-07-06 01:18:23 +0530

    made images 🖼 base64!

--- components/functions/event/deleteEvent/deleteEvent.js
+++ components/functions/event/deleteEvent/deleteEvent.js
@@ -1,42 +1,26 @@
-const Event = require("../../../models/Event/Event")
-const multer = require("multer")
-const fs = require("fs")
-const path = require("path")
-const fileUpload = require("../../upload/fileUpload/fileUpload")
-const fileLimit = require("../../upload/fileFilter/fileFilter")
-const fileStorage = require("../../upload/fileStorage/fileStorage")
+const Event = require("../../../models/Event/Event");
 
-
-
-const deleteEvent= async (req,res) => {
-  const id = req.params.eventId
+const deleteEvent = async (req, res) => {
+  const id = req.params.eventId;
   Event.findById(id).then(async (event) => {
-      if (!event) {
-          return res.status(404).send({
-              message: "event not found"
-          })
-      }
+    if (!event) {
+      return res.status(404).send({
+        message: "event not found",
+      });
+    }
 
-      if (event.creatorId === req.userId) {
-        fs.unlink("public/uploads/eventUpload/"+event.eventImage.substr(44, ),(err) => {
-          if(err){
-            console.log("Error in deleting file")
-            return res.status(404).send({
-              message: "File Not deleted"
-            })
-          }
-          console.log("image of the event removed")
-         })
-          await event.remove()
-          return res.status(200).send({
-              message: "Event deleted Successfully"
-          })
-      } else {
-          return res.status(400).send({
-              message: "You don't have the required privileges to delete this event"
-          })
-      }
-  })
-}
+    if (event.creatorId === req.userId) {
+      //delete the event
+      await event.remove();
+      return res.status(200).send({
+        message: "Event deleted Successfully",
+      });
+    } else {
+      return res.status(400).send({
+        message: "You don't have the required privileges to delete this event",
+      });
+    }
+  });
+};
 
-module.exports = deleteEvent
+module.exports = deleteEvent;
