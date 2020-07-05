1f8acae139e00ca84d07cbd7ef7a26bcc925793f
Vaibhav Chopra <vaibhavchopra@Vaibhavs-MacBook-Air.local>
2020-07-06 01:23:21 +0530

    made images 🖼 base64!

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
 //for parsing various types of requests
 app.use(bodyParser.json());
-app.use(express.urlencoded({
-    extended: true
-}))
-//static folder for event image upload
-app.use('/eventImage', express.static('./public/uploads/eventUpload'))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 // for allowing front end to send requests to api
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
 //for accessing secret variables
-require("dotenv").config()
+require("dotenv").config();
 
 //middleware import for authentication check
-const checkAuth= require("./components/middleware/checkAuth/checkAuth")
-
+const checkAuth = require("./components/middleware/checkAuth/checkAuth");
 
 //importing mongoose and connecting to database
 const mongoose = require("mongoose");
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
 
 //for local testing use: "mongodb://127.0.0.1:27017/hackportal
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
-
-var db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log("db connected")
-
-    //for getting and setting user profiles
-    app.use("/users", checkAuth, require("./components/routes/users/users"))
-
-    //for operations related to events
-    app.use('/events', require("./components/routes/events/events"));
-
-    //for operations related to teams
-    app.use('/teams', require("./components/routes/teams/teams"));
-
-    //for operations related to squads (basically ready made teams)
-    app.use("/squads", require("./components/routes/squads/squads"))
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-const port = process.env.PORT || 3000
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("db connected");
+
+  //for getting and setting user profiles
+  app.use("/users", checkAuth, require("./components/routes/users/users"));
+
+  //for operations related to events
+  app.use("/events", require("./components/routes/events/events"));
+
+  //for operations related to teams
+  app.use("/teams", require("./components/routes/teams/teams"));
+
+  //for operations related to squads (basically ready made teams)
+  app.use("/squads", require("./components/routes/squads/squads"));
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`server started on port ${port}`)
-})
+  console.log(`server started on port ${port}`);
+});
