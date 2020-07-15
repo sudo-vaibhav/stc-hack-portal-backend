f859ba9ba0665332576f9da62de579d89a46b5c4
Vaibhav Chopra <vaibhavchopra@Vaibhavs-MacBook-Air.local>
2020-07-15 20:57:55 +0530

    ⬆️ increased maximum payload size for file uploads (50mb now)

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 
 //for parsing various types of requests
-app.use(bodyParser.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use( bodyParser.json({limit: '50mb'}) );
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit:50000
+}));
+
 
 // for allowing front end to send requests to api
 const cors = require("cors");
