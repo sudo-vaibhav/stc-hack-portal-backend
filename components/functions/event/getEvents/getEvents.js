c2869da364e2763b59ba2dd3b854d8724f18acb4
deepcr7 <deepeshsuranjan@gmail.com>
2020-07-19 00:13:24 +0530

    changed PER_PAGE_LIMIT to 5 from 10

--- components/functions/event/getEvents/getEvents.js
+++ components/functions/event/getEvents/getEvents.js
@@ -1,5 +1,5 @@
 const Event = require("../../../models/Event/Event");
-const PER_PAGE_LIMIT = 10;
+const PER_PAGE_LIMIT = 5;
 const getPaginatedData = require("../../../pagination/getPaginatedData/getPaginatedData");
 const getEvents = async (req, res) => {
   const pageNo = parseInt(req.params.pageNo);
