6d3c1f9a7941592e4d42e3bb17ea663b8773f44f
deepcr7 <deepeshsuranjan@gmail.com>
2020-08-29 20:39:55 +0530

    testing date changes

--- components/models/Event/Event.js
+++ components/models/Event/Event.js
@@ -12,12 +12,12 @@ const EventSchema = mongoose.Schema(
       required: true,
     },
     startDate: {
-      type: String,
+      type: Date,
       required: true,
       validate: /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((1[6-9]|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((1[6-9]|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((1[6-9]|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/,
     },
     endDate: {
-      type: String,
+      type: Date,
       required: true,
       validate: /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((1[6-9]|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((1[6-9]|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((1[6-9]|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/,
     },
@@ -64,5 +64,12 @@ EventSchema.virtual("creator", {
 });
 
 EventSchema.post("remove", EventPostRemove);
+
+EventSchema.post('validate', function (value) {
+  if (this.startDate > this.endDate) {
+    this.invalidate('startDate', 'Start date must be less than end date.', this.startDate);
+  }
+  next();
+});
 
 module.exports = mongoose.model("Event", EventSchema);
