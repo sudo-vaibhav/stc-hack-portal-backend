2838d7907f8520068608a25c08dcaf3a2fbc6e01
Vaibhav <mailvaibhavchopra@gmail.com>
2020-09-08 16:29:52 +0530

    fixed event Update

--- components/functions/event/updateEvent/updateEvent.js
+++ components/functions/event/updateEvent/updateEvent.js
@@ -1,35 +1,28 @@
 const Event = require("../../../models/Event/Event");
 const cleanUserSuppliedInput = require("../../cleanUserSuppliedInput/cleanUserSuppliedInput");
 
-const updateEvent = function (req, res) {
+const updateEvent = async (req, res, next) => {
   const eventData = req.body;
-  delete eventData["_id"];
-  delete eventData["creatorId"];
+
   let dataRecords = cleanUserSuppliedInput(eventData);
 
   const id = req.params.eventId;
-
-  var update = Event.findOneAndUpdate(
-    {
-      _id: id,
-    },
-    dataRecords,
-    {
-      omitUndefined: true,
-    }
-  );
-  update
-    .exec()
-    .then((event) => {
-      return res.status(200).send({
-        message: "Event has been updated",
-      });
-    })
-    .catch((err) => {
-      return res.status(500).send({
-        message: "Internal Server Error",
-      });
-    });
+  try {
+    var update = await Event.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      { $set: dataRecords },
+      {
+        omitUndefined: true,
+        runValidators: true,
+        new: true,
+      }
+    );
+    return res.status(200).send(update);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = updateEvent;
